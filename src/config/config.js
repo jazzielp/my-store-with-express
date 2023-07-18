53791df2735864feb26ea246c7d99240677dccf9
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-07-18 11:31:19 -0700

    Setting the environment variables for the database connection

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const config = {
+  port: process.env.PORT,
+  dbHost: process.env.DB_HOST,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT
+}
+
+module.exports = { config }
--- src/lib/connectionDB.js
+++ src/lib/connectionDB.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2/promise')
 const { createPool } = mysql
 
+const { config } = require('../config/config')
+
 const pool = createPool({
-  host: 'localhost',
-  user: 'admin',
-  password: 'Toluca.01',
-  database: 'db_prueba',
-  port: 3306
+  host: config.dbHost,
+  user: config.dbUser,
+  password: config.dbPassword,
+  database: config.dbName,
+  port: config.dbPort
 })
 
 module.exports = pool

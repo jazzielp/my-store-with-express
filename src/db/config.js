b3dc3c813ff04c48eb6ea0fdff042eaeb3bff636
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-07-19 09:53:37 -0700

    Implementing migrations with Sequelize

--- src/db/config.js
+++ src/db/config.js
@@ -1,19 +1,17 @@
-const { Sequelize } = require('sequelize')
-
 const { config } = require('../config/config')
-const setupModels = require('../db/models')
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
 
 const URL = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-const sequelize = new Sequelize(URL, {
-  dialect: 'mysql',
-  logging: true
-})
-
-setupModels(sequelize)
-// sequelize.sync()
-
-module.exports = sequelize
+module.exports = {
+  development: {
+    url: URL,
+    dialect: 'mysql'
+  },
+  production: {
+    url: URL,
+    dialect: 'mysql'
+  }
+}
